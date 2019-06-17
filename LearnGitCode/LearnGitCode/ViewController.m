39e4cc65ba0ad28c66b4be15d48656c2e08dd8a6
xuetongfei <xuetongfei@58ganji.com>
2019-06-17 17:03:27 +0800

    Revert "NSLog self"
    This reverts commit 474e2f5f6e1de6c82e89047605cedb76c3f5100e.

--- LearnGitCode/LearnGitCode/ViewController.m
+++ LearnGitCode/LearnGitCode/ViewController.m
@@ -20,7 +20,6 @@
     // Do any additional setup after loading the view, typically from a nib.
     printf("maste");
     NSLog(@"%@",self.view);
-    NSLog(@"%@",self);
     
     [TFTimer new];
     
